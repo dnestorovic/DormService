23f838549fd8700bffb02956add6a0d83aa69caf
Natalija9 <asanovic.natalija@gmail.com>
2024-06-02 00:03:05 +0200

    Add repositories

--- DormService/Services/Canteen/Canteen.API/Program.cs
+++ DormService/Services/Canteen/Canteen.API/Program.cs
@@ -1,6 +1,14 @@
+using Canteen.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddScoped<IOrderMealsRepository, OrderMealsRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ DormService/Services/Canteen/Canteen.API/Repositories/IOrderMealsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Canteen.API.Entities;
+
+namespace Canteen.API.Repositories
+{
+    public interface IOrderMealsRepository
+    {
+        Task<OrderMeals> GetOrder(string username);
+        Task<OrderMeals> UpdateOrder(OrderMeals order);
+        Task DeleteOrder(string username);
+    }
+}
