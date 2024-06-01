8ad8216b70235fcf046567e81954081c5b2bd124
Natalija9 <asanovic.natalija@gmail.com>
2024-06-01 19:38:18 +0200

    Add OrderMeals and OrderMealsItem entities

---
+++ DormService/Services/Canteen/Canteen.API/Entities/OrderMeals.cs
@@ -0,0 +1,30 @@
+﻿namespace Canteen.API.Entities
+{
+    public class OrderMeals
+    {
+        public string Username { get; set; }
+        public List<OrderMealsItem> Items { get; set; } = new List<OrderMealsItem>();
+
+        public OrderMeals()
+        {
+        }
+
+        public OrderMeals(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.MealPrice * item.NumberOfMeals;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ DormService/Services/Canteen/Canteen.API/Entities/OrderMealsItem.cs
@@ -0,0 +1,20 @@
+﻿namespace Canteen.API.Entities
+{
+    public class OrderMealsItem
+    {
+        private readonly Dictionary<string, decimal> MealTypeToPrice = new Dictionary<string, decimal>()
+        {
+            {"breakfast", 90}, {"lunch", 120}, {"dinner", 80},
+        };
+
+        public int NumberOfMeals { get; set; }
+        public string MealType { get; set; }
+        public decimal MealPrice
+        {
+            get
+            {
+                return MealTypeToPrice[MealType] = MealPrice;
+            }
+        }
+    }
+}
