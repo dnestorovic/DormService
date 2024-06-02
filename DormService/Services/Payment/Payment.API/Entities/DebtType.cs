f090b7454f7c808913569fffa1244d319f9fbf71
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-02 11:50:13 +0200

    Add classes for Debts

---
+++ DormService/Services/Payment/Payment.API/Entities/DebtType.cs
@@ -0,0 +1,13 @@
+﻿namespace Payment.API.Entities
+{
+    public class DebtType
+    {
+        public static readonly string CREDIT = "Credit";
+        public static readonly string RENT = "Rent";
+        public static readonly string INTERNET = "Internet";
+        public static readonly string AIR_CONDITIONING = "AirConditioning";
+        public static readonly string PHONE = "Phone";
+        public static readonly string CLEANING = "Cleaning";
+        public static readonly List<string> types = [CREDIT, RENT, INTERNET, AIR_CONDITIONING, PHONE, CLEANING];
+    }
+}
---
+++ DormService/Services/Payment/Payment.API/Entities/Debts.cs
@@ -0,0 +1,55 @@
+﻿namespace Payment.API.Entities
+{
+    public class Debts
+    {
+        public string studentID { get; set; }
+
+        Dictionary<string, float> allDebts = new Dictionary<string, float>();
+
+
+
+        public Debts(string studentID, Dictionary<string, float> allDebts)
+        {
+            this.studentID = studentID ?? throw new ArgumentNullException(nameof(studentID));
+            this.allDebts = allDebts ?? throw new ArgumentNullException(nameof(allDebts));
+        }
+
+        public Debts(string studentID)
+        {
+            this.studentID = studentID;
+            foreach (string type in DebtType.types)
+            {
+                allDebts[type] = 0;
+            }
+        }
+
+
+        public Dictionary<string, float> getAllDebts(string studentID)
+        {
+            return allDebts;
+        }
+
+        public Dictionary<string, float> getBasicDebts(string studentID)
+        {
+            Dictionary<string, float> basicDebts = allDebts;
+            basicDebts.Remove(DebtType.CREDIT);
+
+            return basicDebts;
+        }
+
+        public float getCreditStatus(string studentID)
+        {
+            return allDebts[DebtType.CREDIT];
+        }
+
+        public bool reduceCredit(float amount)
+        {
+            if (allDebts[DebtType.CREDIT] >= amount)
+            {
+                allDebts[DebtType.CREDIT] = allDebts[DebtType.CREDIT] - amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
