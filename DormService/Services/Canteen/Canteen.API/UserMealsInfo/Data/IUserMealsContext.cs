9624062c5f4a060c7773d3eff9f95aea0e4b9530
Natalija9 <asanovic.natalija@gmail.com>
2024-06-02 17:57:31 +0200

    Add UserMeals entity, context, repositories and controller

---
+++ DormService/Services/Canteen/Canteen.API/UserMealsInfo/Data/IUserMealsContext.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+using Canteen.API.UserMealsInfo.Entities;
+
+namespace Canteen.API.UserMealsInfo.Data
+{
+    public interface IUserMealsContext
+    {
+        IMongoCollection<UserMeals> Meals { get; }
+    }
+}
