53fae78606d251b725ca5809bc41bfa16f6e6775
dnestorovic <david.nestorovic@gmail.com>
2024-06-05 01:11:23 +0200

    Sync LaundryDayGenerator with available machines from the Laundry room

--- DormService/Services/Laundry/Laundry.API/Data/WashingMachinesReservations/LaundryDayGenerator.cs
+++ DormService/Services/Laundry/Laundry.API/Data/WashingMachinesReservations/LaundryDayGenerator.cs
@@ -5,20 +5,17 @@ namespace Laundry.API.Data;
 public class LaundryDayGenerator
 {
 
-    public static IEnumerable<WashingMachine> CreateNewDefaultDay(string date) 
+    public static IEnumerable<WashingMachine> CreateNewDefaultDay(string date, IEnumerable<WashingMachineConfiguration> availableMachines) 
     {
-        // TODO: Should be replaced with fetching from other source 
-        IEnumerable<WashingMachine> machines = [
-        new WashingMachine(date, TimeSlot.Morning.ToString()),
-        new WashingMachine(date, TimeSlot.Morning.ToString()),
-        new WashingMachine(date, TimeSlot.Morning.ToString()),
-        new WashingMachine(date, TimeSlot.Noon.ToString()),
-        new WashingMachine(date, TimeSlot.Noon.ToString()),
-        new WashingMachine(date, TimeSlot.Noon.ToString()),
-        new WashingMachine(date, TimeSlot.Afternoon.ToString()),
-        new WashingMachine(date, TimeSlot.Afternoon.ToString()),
-        new WashingMachine(date, TimeSlot.Afternoon.ToString())
-    ];
+        List<WashingMachine> machines = new List<WashingMachine>();
+        foreach (TimeSlot timeSlot in Enum.GetValues(typeof(TimeSlot)))
+        {
+            foreach (WashingMachineConfiguration wm in availableMachines)
+            {
+                machines.Add(new WashingMachine(wm._id, date, timeSlot.ToString()));
+            }   
+        }
+
         return machines;
     }
 
