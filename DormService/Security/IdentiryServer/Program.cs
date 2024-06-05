f3e461e81df42abb5a752889089f8f78e922800c
Natalija9 <asanovic.natalija@gmail.com>
2024-06-05 23:16:01 +0200

    Add extensions

--- DormService/Security/IdentiryServer/Program.cs
+++ DormService/Security/IdentiryServer/Program.cs
@@ -1,3 +1,6 @@
+using IdentiryServer.Extensions;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication();
+builder.Services.ConfigurePresistence(builder.Configuration);
+builder.Services.ConfigureIdentity();
+
 
 var app = builder.Build();
 
@@ -16,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
