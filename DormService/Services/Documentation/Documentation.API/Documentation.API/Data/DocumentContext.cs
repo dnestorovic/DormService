879a27483828cf3f54fcbbf73f306fd6f887be48
tekisooj <teodoravasa@gmail.com>
2024-06-06 00:22:25 +0200

    Add document collection

---
+++ DormService/Services/Documentation/Documentation.API/Documentation.API/Data/DocumentContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using Documentation.API.Entities;
+
+
+namespace Documentation.API.Data
+{
+    public class DocumentContext : IDocumentContext
+    {
+        public DocumentContext() { 
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("DocumentationDB");
+
+            Documents = database.GetCollection<Document>("Documents");
+
+        }
+        public IMongoCollection<Document> Documents { get; }
+
+    }
+}
