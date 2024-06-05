f9fa85a3cc728ba0b3fad39ae58c06cee74590d4
David Nestorović <57294550+dnestorovic@users.noreply.github.com>
2024-06-05 20:59:42 +0200

    Sync LaundryDayGenerator with available machines from the Laundry room (#78)

--- DormService/Services/Laundry/Laundry.API/Entities/WashingMachine.cs
+++ DormService/Services/Laundry/Laundry.API/Entities/WashingMachine.cs
@@ -6,13 +6,15 @@ public class WashingMachine
 {
 
     public string _id { get; set; }
+    public string configurationId { get; set; }
     public string? Time { get; set; }
     public string? Date { get; set; }
     public bool Reserved { get; set; } 
 
-    public WashingMachine(string date, string time) 
+    public WashingMachine(string id, string date, string time) 
     {
         _id = ObjectId.GenerateNewId().ToString();
+        configurationId = id;
         Date = date;
         Time = time;
         Reserved = false;
