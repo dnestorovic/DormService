36f39902148ac8784a6e5ef60a136f29839a1dc3
Natalija9 <asanovic.natalija@gmail.com>
2024-06-08 15:02:41 +0200

    Add role configuration

---
+++ DormService/Security/IdentiryServer/Data/EntityTypeConfigutarions/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentiryServer.Data.EntityTypeConfigutarions
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder) 
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "Student",
+                    NormalizedName = "STUDENT"
+                },
+                new IdentityRole
+                {
+                    Name = "Administator",
+                    NormalizedName = "ADMIONISTRATOR"
+                }
+            );
+        }
+    }
+}
