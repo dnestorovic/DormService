fe2e15fa3970d5d055bf76c43eac28184b622bde
Natalija9 <asanovic.natalija@gmail.com>
2024-06-08 19:35:38 +0200

    Create map

---
+++ DormService/Security/IdentiryServer/Mapper/IdentityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using IdentiryServer.DTOs;
+using IdentiryServer.Entities;
+
+namespace IdentiryServer.Mapper
+{
+    public class IdentityProfile : Profile
+    {
+        protected IdentityProfile()
+        {
+            CreateMap<User, NewUserDTO>().ReverseMap();
+        }
+    }
+}
