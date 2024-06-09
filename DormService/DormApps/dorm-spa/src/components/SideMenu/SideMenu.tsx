d4ab8a55004a0156771e3a0f4d84a14f8eed3910
dnestorovic <david.nestorovic@gmail.com>
2024-06-09 16:50:25 +0200

    Add links to all pages in navigation menu

--- DormService/DormApps/dorm-spa/src/components/SideMenu/SideMenu.tsx
+++ DormService/DormApps/dorm-spa/src/components/SideMenu/SideMenu.tsx
@@ -9,26 +9,36 @@ const SideMenu = () => {
             <div className='pattern'>
                 <div className={['title', showMenu ? "show-title" : "hide-title"].join(' ')}><span>Dorm Service</span></div>
                 <ul>
-                    <li className='navigation-item navigation-login'>
-                        <div className='icon' />
-                        <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Login</div>
-                    </li>
-                    <li className='navigation-item navigation-canteen'>
-                        <div className='icon' />
-                        <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Canteen</div>
-                    </li>
-                    <li className='navigation-item navigation-payments'>
-                        <div className='icon' />
-                        <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Payments</div>
-                    </li>
-                    <li className='navigation-item navigation-laundry'>
-                        <div className='icon' />
-                        <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Laundry</div>
-                    </li>
-                    <li className='navigation-item navigation-documentation'>
-                        <div className='icon' />
-                        <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Documentation</div>
-                    </li>
+                    <Link to="/login">
+                        <li className='navigation-item navigation-login'>
+                            <div className='icon' />
+                            <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Login</div>
+                        </li>
+                    </Link>
+                    <Link to="/canteen">
+                        <li className='navigation-item navigation-canteen'>
+                            <div className='icon' />
+                            <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Canteen</div>
+                        </li>
+                    </Link>
+                    <Link to="/payments">
+                        <li className='navigation-item navigation-payments'>
+                            <div className='icon' />
+                            <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Payments</div>
+                        </li>
+                    </Link>
+                    <Link to="/laundry">
+                        <li className='navigation-item navigation-laundry'>
+                            <div className='icon' />
+                            <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Laundry</div>
+                        </li>
+                    </Link>
+                    <Link to="/documentation">
+                        <li className='navigation-item navigation-documentation'>
+                            <div className='icon' />
+                            <div className={['label', showMenu ? "show-label" : "hide-label"].join(' ')}>Documentation</div>
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </div>
