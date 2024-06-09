dde436e537fb3790c90a399cba6fc36eef44529b
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-09 15:42:40 +0200

    Add login endpoint

--- DormService/Security/IdentiryServer/Controllers/AuthenticationController.cs
+++ DormService/Security/IdentiryServer/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using IdentiryServer.Controllers.Base;
 using IdentiryServer.DTOs;
 using IdentiryServer.Entities;
+using IdentiryServer.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,12 @@ namespace IdentiryServer.Controllers
     [ApiController]
     public class AuthenticationController : RegistrationControllerBase
     {
-        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager) 
+        private readonly IAuthenticationService _authService;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger, IMapper mapper, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IAuthenticationService authService)
             : base(logger, mapper, userManager, roleManager)
         {
+            _authService = authService ?? throw new ArgumentNullException(nameof(authService));
         }
 
         [HttpPost("[action]")]
@@ -32,6 +36,21 @@ namespace IdentiryServer.Controllers
         {
             return await RegisterNewUserWithRoles(newUser, new string[] { "Administrator" });
 
+        }
+
+        [HttpPost("[action]")]
+        [ProducesResponseType(typeof(AuthenticationModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromBody] UserCredentialsDTO userCredentials)
+        {
+            var user = await _authService.ValidateUser(userCredentials);
+            if (user == null)
+            {
+                //_logger.LogWarning($"{nameof(Login)}: Authentication failed. Wrong username or password.");
+                return Unauthorized();
+            }
+
+            return Ok(await _authService.CreateAuthenticationModel(user));
         }
     }
 }
