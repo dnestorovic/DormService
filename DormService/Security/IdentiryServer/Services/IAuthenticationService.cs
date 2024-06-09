79fcc8ec390168f5e7bb1fc88c3a38a532df33f2
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-09 15:41:49 +0200

    Add services

---
+++ DormService/Security/IdentiryServer/Services/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using IdentiryServer.DTOs;
+using IdentiryServer.Entities;
+
+namespace IdentiryServer.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<User> ValidateUser(UserCredentialsDTO userCredentials);
+        Task<AuthenticationModel> CreateAuthenticationModel(User user);
+    }
+}
