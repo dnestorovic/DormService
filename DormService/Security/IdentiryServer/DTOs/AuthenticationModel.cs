754d0c83acba9fa4e1f9d917352d710c179556f6
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-09 15:40:25 +0200

    Add DTOs

---
+++ DormService/Security/IdentiryServer/DTOs/AuthenticationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentiryServer.DTOs
+{
+    public class AuthenticationModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ DormService/Security/IdentiryServer/DTOs/UserCredentialsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentiryServer.DTOs
+{
+    public class UserCredentialsDTO
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password name is required")]
+        public string Password { get; set; }
+    }
+}
