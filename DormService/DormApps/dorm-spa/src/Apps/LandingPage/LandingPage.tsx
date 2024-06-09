5a8aea1362bb2250eccfeb7399b765ed9d558fc7
David Nestorović <57294550+dnestorovic@users.noreply.github.com>
2024-06-09 13:54:22 +0200

    Add landing page and banner (#105)

--- DormService/DormApps/dorm-spa/src/Apps/LandingPage/LandingPage.tsx
+++ DormService/DormApps/dorm-spa/src/Apps/LandingPage/LandingPage.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useMount } from 'react-use';
+import Banner from './components/Banner';
+import Login from './components/Login';
 
 const LandingPage = () => {
-    return <div>Hello World</div>
+    const [showBanner, setShowBanner] = useState<boolean>(false);
+
+    useMount(() => {
+        setShowBanner(true);
+        setTimeout(() => {
+            setShowBanner(false);
+        }, 14050);
+    });
+
+    return showBanner ? <Banner /> : <Login />;
 };
 
 export default LandingPage;
