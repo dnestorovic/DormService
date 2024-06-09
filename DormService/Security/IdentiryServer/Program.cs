1cd44ec6d5bdadacf7f0fdc3a141be4ea295d1a4
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-09 15:42:53 +0200

    Update Program.cs

--- DormService/Security/IdentiryServer/Program.cs
+++ DormService/Security/IdentiryServer/Program.cs
@@ -10,10 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication();
+//builder.Services.AddAuthentication();
 builder.Services.ConfigurePresistence(builder.Configuration);
 builder.Services.ConfigureIdentity();
 
+builder.Services.ConfigureJWT(builder.Configuration);
 builder.Services.ConfigureMiscellaneousServices();
 
 var app = builder.Build();
