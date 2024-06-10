0763c7f116679012876d0436a832a11ac29548c1
Natalija9 <asanovic.natalija@gmail.com>
2024-06-10 21:48:46 +0200

    Add GetUser and GetAllUsers endpoints

---
+++ DormService/Security/IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using IdentityServer.DTOs;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public UserController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<UserDetails>), StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<UserDetails>>> GetAllUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return Ok(_mapper.Map<IEnumerable<UserDetails>>(users));
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("{username}")]
+        [ProducesResponseType(typeof(UserDetails), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserDetails>> GetUser(string username)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(user => user.UserName == username);
+
+            return Ok(_mapper.Map<UserDetails>(user));
+        }
+    }
+}
