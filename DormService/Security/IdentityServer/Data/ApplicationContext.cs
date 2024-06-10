94615bd173ccbeffbefad47a6afc9d96ee779c89
Natalija9 <asanovic.natalija@gmail.com>
2024-06-10 20:32:38 +0200

    Rename folder

---
+++ DormService/Security/IdentityServer/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentiryServer.Data.EntityTypeConfigurations;
+using IdentityServer.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
+    }
+}
