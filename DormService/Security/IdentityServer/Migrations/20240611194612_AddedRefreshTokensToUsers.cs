5039c9f9fe1ae76d5fdf0fb1691c5bff15b714c3
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-11 21:55:07 +0200

    Add refresh token to database
    New migration created

---
+++ DormService/Security/IdentityServer/Migrations/20240611194612_AddedRefreshTokensToUsers.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IdentiryServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRefreshTokensToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3c511db5-8b5c-4081-935e-34dbb4ec602f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dbb5f4e8-a9b9-485b-9b2e-d0cf94244018");
+
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExpiryTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2beba1f6-48c3-4689-9720-f710d15653c2", null, "Administrator", "ADMINISTRATOR" },
+                    { "672c499f-d9fb-4fb0-a24c-bbe28b2cd119", null, "Student", "STUDENT" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshTokens",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2beba1f6-48c3-4689-9720-f710d15653c2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "672c499f-d9fb-4fb0-a24c-bbe28b2cd119");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3c511db5-8b5c-4081-935e-34dbb4ec602f", null, "Student", "STUDENT" },
+                    { "dbb5f4e8-a9b9-485b-9b2e-d0cf94244018", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+    }
+}
