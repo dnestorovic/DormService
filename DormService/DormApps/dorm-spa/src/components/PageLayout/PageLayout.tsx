4738c9b1acb4525872803b0079cb996ae38fc83c
David Nestorović <57294550+dnestorovic@users.noreply.github.com>
2024-06-13 20:21:32 +0200

    Add page transition (#117)

--- DormService/DormApps/dorm-spa/src/components/PageLayout/PageLayout.tsx
+++ DormService/DormApps/dorm-spa/src/components/PageLayout/PageLayout.tsx
@@ -1,16 +1,42 @@
-import React, {ReactNode} from 'react'
+import React, {ReactNode, useEffect, useState} from 'react'
 import SideMenu from '../SideMenu/SideMenu'
+import { useMount } from 'react-use'
 
-type Props = { 
+type Props = {
+    coverPhoto: string,
     children : ReactNode
  }
 
 
 export const PageLayout = (props: Props) => {
+  const [showIntro, setShowIntro] = useState<boolean>(false);
+
+    useEffect(() => {
+        setShowIntro(true);
+        setTimeout(() => {
+            setShowIntro(false);
+        }, 5600);
+    }, [props.coverPhoto]);
+
+    function Capitalize(word : string){
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
   return (
     <div className='page-layout'>
         <SideMenu />
-        <div className='page-content'>{props.children}</div>
+        {showIntro ? <div className={["page-intro", props.coverPhoto].join(" ")}>
+          <div className='shadow'>
+              <h1 className='title'>{Capitalize(props.coverPhoto)}</h1>
+          </div>
+        </div> : 
+        <div className="page-content">
+          <div className='shadow'>
+            <div className='child-content'>{props.children}</div>
+          </div>
+        </div>
+        
+        }
     </div>
   )
 }
