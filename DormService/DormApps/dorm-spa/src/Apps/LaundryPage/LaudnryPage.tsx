d4a08555267261660ff79c0605b65667ef71af88
dnestorovic <david.nestorovic@gmail.com>
2024-06-17 22:15:50 +0200

    Add laundry page and style

--- DormService/DormApps/dorm-spa/src/Apps/LaundryPage/LaudnryPage.tsx
+++ DormService/DormApps/dorm-spa/src/Apps/LaundryPage/LaudnryPage.tsx
@@ -1,31 +1,59 @@
 import React, { useState } from 'react'
 import { ModalDialog } from '../../components/Modals/ModalDialog';
+import DropdownMenu from './components/DropdownMenu/DropdownMenu';
+import { useMount } from 'react-use';
+import { Timeframes, WashingMachine } from './models/WashingMachine';
+import UserReservationsService from './services/UserReservationsService';
 
 export default function LaudnryPage() {
 
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showNotification, setShowNotification] = useState<{type?: string, message?: string}>({});
 
-  const handleClick = () => {
-      setShowModal(prev => true);
+
+  const [selectedDate, setSelectedDate] = useState<string>();
+  const [selectedTime, setSelectedTime] = useState<string>();
+  const [allDates, setAllDates] = useState<string[]>([]);
+
+  const [availabelMachines, setAvailabelMachines] = useState<WashingMachine[]>([]);
+  
+
+  const selectDate = (date: string) => {
+    setSelectedDate(prev => date);
+    UserReservationsService.getWashingMachinesByDate(date)
+      .then(machines => setAvailabelMachines([...machines]))
+      .catch(() => setShowNotification({type: "error", message: "Machines for the given date not availabel"}))
   }
 
-  const onSubmitClick = () => {
-      alert("Clicked on submit");
+  const selectPeriod = (time: string) => {
+      setSelectedTime(prev => time);
   }
 
-  const onCancelClick = () => {
-      alert("Clicked on cancel");
-      setShowModal(prev => false);
-  }
+  useMount(() => {
+    var availableDates: string[] = [];
+    for (let index = 0; index < 7; index++) {
+      var nextDay = new Date(Date.now() + index * 24 * 60 * 60 * 1000);
+      availableDates = [...availableDates, nextDay.toLocaleDateString('en-UK')]
+    }
 
+    setAllDates(prev => [...availableDates])
+  });
 
   return (
-    <>
-      <button onClick={handleClick}>LaudnryPage</button>
-      <button onClick={() => alert("Test Click!")}>Test click</button>
-      {showModal && <ModalDialog header='Test dynamic header' submitText='Custom submit' cancelText='Custom cancel' onCancel={onCancelClick} onSubmit={onSubmitClick}>
-        <p>This is an example of a modal dialog with configurable header, content and actions</p>  
-      </ModalDialog>}
-    </>
+    <div className='laundry-page'>
+      <div className='left-panel panel'>
+        <h1>Reservations</h1>
+        <div className='reservation-form'>
+            <div className='selector'>
+              <DropdownMenu title={selectedDate || "Select date"} options={allDates} onSelect={selectDate}/>
+              <DropdownMenu title={selectedTime || "Select time"} options={Object.values(Timeframes)} onSelect={selectPeriod}/>
+            </div>
+            <div className='machines'>
+                {selectedDate && selectedTime ? availabelMachines.map(m => <span>{m._id}</span>) : null}
+            </div>
+        </div>
+      </div>
+      <div className='right-panel panel'>B</div>
+    </div>
   )
 }
---
+++ DormService/DormApps/dorm-spa/src/Apps/LaundryPage/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type DropdownMenuProps = {
+  options: string[];
+  title?: string;
+  onSelect: (option: string) => void; 
+}
+
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ title, options, onSelect }) =>{
+  return (
+    <div className='dropdown-menu'>
+        <div className="dropdown">
+        <div className="dropdown-button">{title || "Select an option"}<span>&#x25BE;</span></div>
+        <ul className="dropdown-list">
+            {options.map(op => {
+              return <li key={op} onClick={() => onSelect(op)}>{op}</li>
+            })}
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default DropdownMenu;
