163ee0b047c13f9e32cde750e49476db328cb0bb
David Nestorović <57294550+dnestorovic@users.noreply.github.com>
2024-06-17 23:26:10 +0200

    Laundry service cleanup (#140)
    * Unify paths
    
    * Add cors policy to Laundry service
    
    * Add studentId in reservation mechanism

--- DormService/Services/Laundry/Laundry.API/Program.cs
+++ DormService/Services/Laundry/Laundry.API/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddGrpcClient<PaymentProtoService.PaymentProtoServiceClient>(
     options => options.Address = new Uri(builder.Configuration["GrpcSettings:PaymentUrl"]));
 builder.Services.AddScoped<PaymentGrpcService>();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
