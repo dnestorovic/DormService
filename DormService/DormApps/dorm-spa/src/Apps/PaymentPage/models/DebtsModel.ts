eded01bff02b140f4c8ddeebb401c438ccf22348
Momcilo Knezevic <moma.knezevic7@gmail.com>
2024-06-20 18:18:34 +0200

    Create constant for default student debts

--- DormService/DormApps/dorm-spa/src/Apps/PaymentPage/models/DebtsModel.ts
+++ DormService/DormApps/dorm-spa/src/Apps/PaymentPage/models/DebtsModel.ts
@@ -8,3 +8,14 @@ export type StudentDebts =
     phone: number,
     cleaning: number
 }
+
+export const DefaultStudentDebts =
+{
+    studentID: "",
+    credit: 0,
+    rent: 0,
+    internet: 0,
+    airConditioning: 0,
+    phone: 0,
+    cleaning: 0
+}
