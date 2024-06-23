0f07c149e9a2f7d158ff94b08e7a868549189b16
Natalija9 <asanovic.natalija@gmail.com>
2024-06-23 14:51:32 +0200

    Create separate page for registration

--- DormService/DormApps/dorm-spa/src/Apps/LandingPage/components/Login.tsx
+++ DormService/DormApps/dorm-spa/src/Apps/LandingPage/components/Login.tsx
@@ -4,20 +4,12 @@ import { Credentials, User } from "../../../models/User";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const [registered, setRegistered] = useState(false);
     const [credentials, setCredentials] = useState<Credentials>({userName:"", password:""});
-    const [registerUser, setRegisterUser] = useState<User>({});
-
-    const message = "Already registered? Log in ";
 
     const navigate = useNavigate();
 
     const handleInput = (value: string, prop: keyof Credentials) => {
         setCredentials((prev) => ({ ...prev, [prop]: value }));
-    };
-
-    const handleRegisterInput = (value: string, prop: keyof User) => {
-        setRegisterUser((prev) => ({ ...prev, [prop]: value }));
     };
 
     const handleLogin = () => {
@@ -41,119 +33,36 @@ export default function Login() {
             .catch(() => alert("Cannot find user with this credentials."));
     };
 
-    const handleRegister = () => {
-        console.log(registerUser);
-        if (registerUser.password === undefined 
-            || registerUser.userName === undefined
-            || registerUser.firstName === undefined
-            || registerUser.lastName === undefined
-            || registerUser.email === undefined
-        ) {
-            alert("All fields must be populated");
-            return;
-        }
-
-        IdentityService.register(registerUser)
-            .then(() => {
-                setRegisterUser({});
-                setCredentials({});
-                setRegistered(true);
-            })
-            .catch(() => alert("Something went wrong!"));
-    };
-
     return (
         <div className="login">
             <div className='image-shadow'>
                 
-                {registered && 
-                    <div className="login-dialog">
-                        <div>
-                            <h2>{"Log in"}</h2>
-                        </div>
-                        <div className="form-field">
-                            <label>Username:</label>
-                            <input
-                                value={credentials.userName}
-                                onChange={(event) => handleInput(event.currentTarget.value, 'userName')}
-                            />
-                        </div>
-                        <div className="form-field">
-                            <label>Password:</label>
-                            <input
-                                type="password"
-                                value={credentials.password}
-                                onChange={(event) => handleInput(event.currentTarget.value, 'password')}
-                            />
-                        </div>
-                        <footer>
-                            <button className="btn-submit" onClick={handleLogin}>
-                                <span>Submit</span>
-                            </button>
-                        </footer>
+                <div className="login-dialog">
+                    <div>
+                        <h2>{"Log in"}</h2>
                     </div>
-                }
-                {!registered &&
-                    <div className="register-dialog">
-                        <div>
-                            <h2>{"Register"}</h2>
-                        </div>
-                        <div className="form-field">
-                            <label>First name:</label>
-                            <input
-                                value={registerUser.firstName}
-                                onChange={(event) => handleRegisterInput(event.currentTarget.value, 'firstName')}
-                            />
-                        </div>
-                        <div className="form-field">
-                            <label>Last name:</label>
-                            <input
-                                value={registerUser.lastName}
-                                onChange={(event) => handleRegisterInput(event.currentTarget.value, 'lastName')}
-                            />
-                        </div>
-                        <div className="form-field">
-                            <label>Username:</label>
-                            <input
-                                value={registerUser.userName}
-                                onChange={(event) => handleRegisterInput(event.currentTarget.value, 'userName')}
-                            />
-                        </div>
-                        <div className="form-field">
-                            <label>Password:</label>
-                            <input
-                                type="password"
-                                value={registerUser.password}
-                                onChange={(event) => handleRegisterInput(event.currentTarget.value, 'password')}
-                            />
-                        </div>
-                        <div className="form-field">
-                            <label>Email:</label>
-                            <input
-                                type="email"
-                                value={registerUser.email}
-                                onChange={(event) => handleRegisterInput(event.currentTarget.value, 'email')}
-                            />
-                        </div>
-                        <footer>
-                            <button className="btn-submit" onClick={handleRegister}>
-                                <span>Submit</span>
-                            </button>
-                        </footer>
-                        <p>
-                            {message}
-                            <span
-                                onClick={() => {
-                                    if (!registered) {
-                                        setRegistered(true);
-                                    }
-                                }}
-                            >
-                            here.
-                        </span>
-                    </p>
+                    <div className="form-field">
+                        <label>Username:</label>
+                        <input
+                            value={credentials.userName}
+                            onChange={(event) => handleInput(event.currentTarget.value, 'userName')}
+                        />
                     </div>
-                }
+                    <div className="form-field">
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            value={credentials.password}
+                            onChange={(event) => handleInput(event.currentTarget.value, 'password')}
+                        />
+                    </div>
+                    <footer>
+                        <button className="btn-submit" onClick={handleLogin}>
+                            <span>Submit</span>
+                        </button>
+                    </footer>
+                </div>
+               
             </div>
         </div>
     )
